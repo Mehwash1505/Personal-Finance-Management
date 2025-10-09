3c1d7bc0b2684043258cb9caf0c39cab119e0677
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-10 01:30:37 +0530

    connection built

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { testUserEndpoint } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  testUserEndpoint,
+} = require('../controllers/userController');
 
-// This will be reachable at http://localhost:5001/api/users/test
-router.get('/test', testUserEndpoint);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/test', testUserEndpoint); // Kept for testing
 
 module.exports = router;
