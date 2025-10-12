98258b39dedd68b080dd61f68e98e080ed0a8ae9
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-13 00:12:01 +0530

    routes connected to frontend

--- backend/routes/plaidRoutes.js
+++ backend/routes/plaidRoutes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const {
   createLinkToken,
-  exchangePublicToken, // Import new functions
+  exchangePublicToken, 
   getTransactions,
+  getAccounts,
 } = require('../controllers/plaidController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/create_link_token', protect, createLinkToken);
 
-// --- ADD THE TWO NEW ROUTES BELOW ---
+
 router.post('/exchange_public_token', protect, exchangePublicToken);
 router.get('/transactions', protect, getTransactions);
+router.get('/accounts', protect, getAccounts);
 
 module.exports = router;
