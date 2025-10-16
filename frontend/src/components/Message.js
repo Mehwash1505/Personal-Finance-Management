7da9669e14e05a16a678074cbd69858887bea71a
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 03:35:57 +0530

    messages to be displayed

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,15 @@
+// File: src/components/Message.js
+import React from 'react';
+
+const Message = ({ type = 'error', children }) => {
+  const bgColor = type === 'error' ? 'bg-red-100' : 'bg-green-100';
+  const textColor = type === 'error' ? 'text-red-700' : 'text-green-700';
+
+  return (
+    <div className={`${bgColor} ${textColor} p-3 rounded-md my-4`}>
+      {children}
+    </div>
+  );
+};
+
+export default Message;
