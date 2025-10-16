c8c97d1bd3b12b7e53804c90fec332f66a0bcfec
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 03:05:22 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'; // <-- Import Header
+import Navbar from './components/Header'; // <-- Import Header
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -9,7 +9,7 @@ import DashboardPage from './pages/DashboardPage';
 function App() {
   return (
     <Router>
-      <Header /> {/* <-- Add Header here */}
+      <Navbar /> {/* <-- Add Header here */}
       <div className="container mx-auto p-4">
         <Routes>
           <Route path="/" element={<HomePage />} />
