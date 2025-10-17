49b41c47501121d391b61e1589e32dc8a62e20c5
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 18:24:39 +0530

    manual transactions can be added

---
+++ backend/models/Transaction.js
@@ -0,0 +1,33 @@
+// File: backend/models/Transaction.js
+const mongoose = require('mongoose');
+
+const transactionSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: [true, 'Please add a transaction name'],
+    },
+    amount: {
+      type: Number,
+      required: [true, 'Please add an amount'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Please add a category'],
+    },
+    type: {
+      type: String, // 'income' or 'expense'
+      required: [true, 'Please specify the type'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Transaction', transactionSchema);
