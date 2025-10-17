02242903a04f4ddf98fb65d0e877eafa784b3506
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 17:26:24 +0530

    wrapping up public route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute'; // <-- 1. Import PublicRoute
 
 function App() {
   return (
@@ -12,10 +14,17 @@ function App() {
       <Navbar />
       <main className="container mx-auto p-4">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          {/* --- 2. Wrap Public Routes --- */}
+          <Route path="/" element={<PublicRoute />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          {/* --- Protected Route --- */}
+          <Route path="/dashboard" element={<ProtectedRoute />}>
+            <Route index element={<DashboardPage />} />
+          </Route>
         </Routes>
       </main>
     </Router>
