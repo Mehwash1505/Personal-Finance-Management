3da63ebb44523aa62d9c31e3734d20c398dc60c3
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 16:55:24 +0530

    Navbar modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Header'; // <-- Import Header
+import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -9,15 +9,15 @@ import DashboardPage from './pages/DashboardPage';
 function App() {
   return (
     <Router>
-      <Navbar /> {/* <-- Add Header here */}
-      <div className="container mx-auto p-4">
+      <Navbar />
+      <main className="container mx-auto p-4">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
-      </div>
+      </main>
     </Router>
   );
 }
