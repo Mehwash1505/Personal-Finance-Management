9e405089f618936e7aa3df9952de57c122520104
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-17 18:57:24 +0530

    modifying server

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,8 @@ app.use('/api/transactions', transactionRoutes);
 const PORT = process.env.PORT || 5000;
 
 // Start the server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = { app, server };
