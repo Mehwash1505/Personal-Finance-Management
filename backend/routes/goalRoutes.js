e11fbf7591c01952ad3e8fdb2fdcdac699230057
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-18 23:38:18 +0530

    goal route created

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getGoals, createGoal, updateGoal } = require('../controllers/goalController');
+const { protect } = require('../middlewares/authMiddleware');
+
+router.route('/').get(protect, getGoals).post(protect, createGoal);
+router.route('/:id').put(protect, updateGoal);
+
+module.exports = router;
