d4855d1b3d4c00860d496c6d22b6a0c438857ec8
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-18 23:40:12 +0530

    update profile added to user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,13 @@ const {
   registerUser,
   loginUser,
   testUserEndpoint,
+  updateUserProfile,
 } = require('../controllers/userController');
+const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/test', testUserEndpoint); // Kept for testing
+router.route('/profile').put(protect, updateUserProfile);
 
 module.exports = router;
