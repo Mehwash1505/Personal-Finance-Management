45c696fabc8147e7d53d1bbaab4bada5f33d8f54
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-28 23:59:24 +0530

    bill schema added

---
+++ backend/models/Bill.js
@@ -0,0 +1,37 @@
+// File: backend/models/Bill.js
+const mongoose = require('mongoose');
+
+const billSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: [true, 'Please add a bill name'],
+    },
+    amount: {
+      type: Number,
+      required: [true, 'Please add an amount'],
+    },
+    dueDate: {
+      type: Date,
+      required: [true, 'Please add a due date'],
+    },
+    isRecurring: {
+      type: Boolean,
+      default: false,
+    },
+    category: {
+        type: String,
+        default: 'Utilities'
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Bill', billSchema);
