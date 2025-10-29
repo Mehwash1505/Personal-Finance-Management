7ab9e43e172358711ef28046c8487c9e76000f44
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-30 01:19:48 +0530

    bill route created

---
+++ backend/routes/billRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const { getBills, addBill } = require('../controllers/billController');
+const { protect } = require('../middlewares/authMiddleware');
+
+router.route('/').get(protect, getBills).post(protect, addBill);
+
+module.exports = router;
