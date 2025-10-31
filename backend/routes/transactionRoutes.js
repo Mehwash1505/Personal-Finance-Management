d381becdd41a27bd92079cd8b86a317d06208f44
Mehwash Nasir <asmanasreen388@gmail.com>
2025-10-31 22:15:31 +0530

    to export transactions

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,9 +1,9 @@
-// File: backend/routes/transactionRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getTransactions, addTransaction } = require('../controllers/transactionController');
+const { getTransactions, addTransaction, exportTransactions } = require('../controllers/transactionController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.route('/').get(protect, getTransactions).post(protect, addTransaction);
+router.route('/export').get(protect, exportTransactions); // <-- Ensure this line is present
 
 module.exports = router;
