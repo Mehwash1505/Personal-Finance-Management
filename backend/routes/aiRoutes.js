43fa8697222687fd39eba2bc4aee792f96e5e224
Mehwash Nasir <asmanasreen388@gmail.com>
2025-11-02 17:35:37 +0530

    ai route added

---
+++ backend/routes/aiRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { askAiAdvisor } = require('../controllers/aiController');
+const { protect } = require('../middlewares/authMiddleware');
+
+router.post('/ask', protect, askAiAdvisor);
+
+module.exports = router;
