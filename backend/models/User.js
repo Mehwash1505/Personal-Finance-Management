c94c8f3e69da386569d6193e6a2a48dd5d0ad373
Mehwash1505 <asmanasreen388@gmail.com>
2026-01-17 22:13:02 +0530

    user schema corrected

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Please add a name'],
-    },
+    }, 
     email: {
       type: String,
       required: [true, 'Please add an email'],
