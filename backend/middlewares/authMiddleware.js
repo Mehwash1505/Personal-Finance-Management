d05818fd8f810dd78471878f517f5978c29c45d5
Mehwash1505 <asmanasreen388@gmail.com>
2026-01-20 23:11:30 +0530

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
  
-const protect = async (req, res, next) => {
+const protect = async (req, res, next) => { 
   let token;
 
   // First, try to get the token from the standard Authorization header
