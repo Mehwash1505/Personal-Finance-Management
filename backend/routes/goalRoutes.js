2e25c310c090f732b981c249107cc8af1eadd0c0
Mehwash1505 <asmanasreen388@gmail.com>
2026-01-26 23:38:22 +0530

    modification for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,3 +1,4 @@
+// File: backend/routes/goalRoutes.js
 const express = require('express');
 const router = express.Router();
 const { getGoals, createGoal, updateGoal } = require('../controllers/goalController');
